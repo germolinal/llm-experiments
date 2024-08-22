4e97c8c1c6ea88d7393298d319330cfb7ce1be07
German Molina <germolinal@gmail.com>
2024-08-22 19:24:18 +1200

    better landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,36 +3,38 @@ import styles from './page.module.css'
 function Project ({
   title,
   description,
-  link,
-  requiresApiKey
+  link
 }: {
   title: string
   description: string
   link: string
-  requiresApiKey: boolean
 }) {
   return (
-    <li>
-      <a href={link}>
-        <div>
-          <span>{title}</span>
-          <span>{description}</span>
-          <span>{requiresApiKey ? '*' : ''}</span>
-        </div>
-      </a>
-    </li>
+    <a href={link}>
+      <div className={styles.project}>
+        <span>{title}</span>
+        <span>{description}</span>
+      </div>
+    </a>
   )
 }
 
 export default function Home () {
   return (
-    <ul className={styles.ul}>
-      <Project
-        title='Colour palette generator'
-        description='asdasd'
-        link='palette'
-        requiresApiKey={true}
-      />
-    </ul>
+    <div className={styles.projects}>
+      <p>Conversational experiences</p>
+      <div>
+        <Project title='Chat' description='Simple chat interface' link='chat' />
+      </div>
+      <p>Client-side AI</p>
+      <div>
+        <Project
+          title='Colour palette generator'
+          description='Demo of an on-device AI'
+          link='palette'
+        />
+
+      </div>
+    </div>
   )
 }
